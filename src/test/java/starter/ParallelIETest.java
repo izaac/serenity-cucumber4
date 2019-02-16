24e59d9d2bc779deb737c99a973255bc0ab5e94a
Izaac Zavaleta <jorge.izaac@gmail.com>
2019-02-15 23:22:13 -0700

    adding first commit of serenity with cucumber 4 support

---
+++ src/test/java/starter/ParallelIETest.java
@@ -0,0 +1,13 @@
+package starter;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features="src/test/resources/features/search/search_by_keyword.feature"
+//        features = "classpath:features"
+)
+public class ParallelIETest extends BrowserStackSerenityTest {}
